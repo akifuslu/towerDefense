058669300f33070c55976d894fef1c3d35b26daa
cahity <cw.cahity@gmail.com>
2019-02-22 22:36:29 +0300

    GameEntity Started

--- TowerDefense/main.cpp
+++ TowerDefense/main.cpp
@@ -19,10 +19,9 @@ int main()
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		DrawTextureEx(sky, {0,0}, 0, 2, WHITE);
-		DrawText(std::to_string(i++).c_str(), 200, 20, 200, BLUE);
-		DrawTextureEx(cloud, { 100,100 }, 0, 0.7, WHITE);
-		DrawTextureEx(cloud, { 600,30 }, 0, 0.7, WHITE);
-		DrawTextureEx(sword, { 450,275 }, 0, 1.5, WHITE);
+		DrawTextureEx(cloud, { 100,100 }, 0, (float)0.7, WHITE);
+		DrawTextureEx(cloud, { 600,30 }, 0, (float)0.7, WHITE);
+		DrawTextureEx(sword, { 450,275 }, 0, (float)1.5, WHITE);
 		DrawText("Tower Defense!!", 400, 256, 30, RED);
 		EndDrawing();
 	}
