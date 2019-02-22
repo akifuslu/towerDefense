fa4d1ca1f3c909fca16325cd594cef5e8af99fac
akifuslu <40760783+akifuslu@users.noreply.github.com>
2019-02-22 19:17:09 +0300

    Texture Loading and some other tests

--- TowerDefense/main.cpp
+++ TowerDefense/main.cpp
@@ -4,9 +4,12 @@
 int main()
 {
 	int screenWidth = 1024;
-	int screenHeight = 768;
+	int screenHeight = 512;
 
 	InitWindow(screenWidth, screenHeight, "Tower Defense Pre-Alpha");
+	Texture2D sky = LoadTexture("Resources\\sky.png");
+	Texture2D cloud = LoadTexture("Resources\\cloud.png");
+	Texture2D sword = LoadTexture("Resources\\sword.png");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
@@ -16,7 +19,11 @@ int main()
 		//Draw
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawText("Tower Defense!!", 512, 384, 30, RED);
+		DrawTextureEx(sky, {0,0}, 0, 2, WHITE);
+		DrawTextureEx(cloud, { 100,100 }, 0, 0.7, WHITE);
+		DrawTextureEx(cloud, { 600,30 }, 0, 0.7, WHITE);
+		DrawTextureEx(sword, { 450,275 }, 0, 1.5, WHITE);
+		DrawText("Tower Defense!!", 400, 256, 30, RED);
 		EndDrawing();
 	}
 
