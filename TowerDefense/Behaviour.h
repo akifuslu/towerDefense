b64078e5452ea360973bbe42dbe36c420b56c8c8
cahity <cw.cahity@gmail.com>
2019-02-26 16:03:48 +0300

    Behaviour Class & Entity status

--- TowerDefense/Behaviour.h
+++ TowerDefense/Behaviour.h
@@ -3,11 +3,15 @@
 #include "pch.h"
 #include "GameEntity.h"
 
+//Singleton behaviour class for updating towers and mobs
 class Behaviour
 {
 private:
 	Behaviour() {};
+
+	//Entities we want to control
 	std::vector<GameEntity*> entities;
+
 public:
 	Behaviour(Behaviour const&) = delete;
 	void operator=(Behaviour const&) = delete;
@@ -17,7 +21,9 @@ public:
 		return instance;
 	}
 	
+	//Registering an entity to entity vector
 	void Register(GameEntity& entity);
 
+	//Updating the registered entities
 	void Update();
 };
